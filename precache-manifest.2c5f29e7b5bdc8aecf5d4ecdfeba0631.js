5d82c084c228add7fd180615633424604dbbacc2
Luis Freitas <luis.miguel.freitas@gmail.com>
2019-05-06 18:40:11 +0100

    Updates

---
+++ precache-manifest.2c5f29e7b5bdc8aecf5d4ecdfeba0631.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "245e09e0073190727b5b1cbe0cc92023",
+    "url": "/index.html"
+  },
+  {
+    "revision": "498b1de9db157bb4a316",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "20e93bbe4fc5f70d70ec",
+    "url": "/static/css/main.769b22ed.chunk.css"
+  },
+  {
+    "revision": "498b1de9db157bb4a316",
+    "url": "/static/js/2.49c1f101.chunk.js"
+  },
+  {
+    "revision": "20e93bbe4fc5f70d70ec",
+    "url": "/static/js/main.164657c6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "35f31c7aa7cc82eaec5d5c37ef2de044",
+    "url": "/static/media/STARWARS.35f31c7a.woff"
+  },
+  {
+    "revision": "8b4b05ca1f2673bbea61ad1f04d07f00",
+    "url": "/static/media/Spinner.8b4b05ca.svg"
+  }
+]);
